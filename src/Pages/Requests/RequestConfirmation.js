dbf6acc2f2293e97b163ec79ec6fc32ed7078f90
Casey Poon <caseypoon2020@u.northwestern.edu>
2020-03-10 23:18:15 -0500

    finished request approval page

--- src/Pages/Requests/RequestConfirmation.js
+++ src/Pages/Requests/RequestConfirmation.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link} from 'react-router-dom';
+import { Header, Segment, Button, Icon, Container } from 'semantic-ui-react';
 
 const RequestConfirmation = () => {
 
   return (
-    <div>Hello World</div>
+    <Container style={{ marginTop: "15px"}}>
+      <Header as="h1" icon>
+        <Icon color="yellow" name='check circle' />
+        Game on!
+        <Header.Subheader>
+        Maria Woods has been notified for yor approval!
+        </Header.Subheader>
+        <Header.Subheader>
+        {'Meet at the Barnes & Noble on Church Street.'}
+        </Header.Subheader>
+        <Header.Subheader>
+          Happy playing!
+        </Header.Subheader>
+      </Header>
+      <br />
+      <br />
+      <Button content="Back to Listings" as={Link} to="/" fluid color="yellow" />
+    </Container>
   );
 };
 
